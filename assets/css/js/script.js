77f8f5e93e382452ff45aaddefb41e5e63d7f433
Oluwapelumi99 <oluwajosephine7@gmail.com>
2024-04-13 12:22:40 +0000

    Add game functions

---
+++ assets/css/js/script.js
@@ -0,0 +1,32 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWronganswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
