6d6ef10c5acc767bed9bbe4b0691f422482090c3
Oluwapelumi99 <oluwajosephine7@gmail.com>
2024-04-15 15:17:52 +0000

    Add subtraction code

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -28,7 +28,12 @@ function runGame(gameType) {
     else if (gameType === "multiply") {
      displayMultiplyQuestion(num1, num2);
     }
-    else {
+    else if (gameType === "subtract") {
+     displaySubtractQuestion(num1, num2);
+    }
+     else
+    
+     {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -65,7 +70,9 @@ if (operator === "+") {
     return [operand1 + operand2, "addition"];
 } else if (operator === "x") {
     return [operand1 * operand2, "multiply"];
-}
+}  else if (operator === "-") {
+    return [operand1 - operand2, "subtract"];
+}    
  else {
     alert(`Unimplemented operator ${operator}`);
     throw`Unimplemented operator ${operator}.Aborting!`;
@@ -92,8 +99,10 @@ function displayAdditionQuestion(operand1, operand2) {
  document.getElementById('operator').textContent = "+";
 }
 
-function displaySubtractQuestion() {
-
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
